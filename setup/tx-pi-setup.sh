9bd52d6edb523bf41bb58157c10a816190fef56d
Till Harbaum <till@harbaum.org>
2018-02-12 14:34:52 +0100

    Disable broken i2c-tiny-usb build

--- setup/tx-pi-setup.sh
+++ setup/tx-pi-setup.sh
@@ -425,28 +425,29 @@ EOF
 systemctl daemon-reload
 systemctl enable netreq
 
-
 # build and install i2c-tiny-usb kernel module
-apt-get -y install raspberrypi-kernel-headers
-wget -N https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source
-chmod +x /usr/bin/rpi-source
-/usr/bin/rpi-source -q --tag-update
-apt-get -y install bc
-rpi-source
+# This doesn't work at this stage since the newly istalled
+# kernel isn't running yet
+#apt-get -y install raspberrypi-kernel-headers
+#wget -N https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source
+#chmod +x /usr/bin/rpi-source
+#/usr/bin/rpi-source -q --tag-update
+#apt-get -y install bc
+#rpi-source
 
-mkdir i2c-tiny-usb
-cd i2c-tiny-usb
-echo -e "obj-m += i2c-tiny-usb.o" > Makefile
-echo -e "\nall:" >> Makefile
-echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) modules" >> Makefile
-echo -e "\ninstall:" >> Makefile
-echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) modules_install" >> Makefile
-echo -e "\nclean:" >> Makefile
-echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) clean" >> Makefile
-cp ~/linux/drivers/i2c/busses/i2c-tiny-usb.c .
-make
-make install
-depmod -a
+#mkdir i2c-tiny-usb
+#cd i2c-tiny-usb
+#echo -e "obj-m += i2c-tiny-usb.o" > Makefile
+#echo -e "\nall:" >> Makefile
+#echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) modules" >> Makefile
+#echo -e "\ninstall:" >> Makefile
+#echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) modules_install" >> Makefile
+#echo -e "\nclean:" >> Makefile
+#echo -e "\tmake -C /lib/modules/\$(shell uname -r)/build M=\$(PWD) clean" >> Makefile
+#cp ~/linux/drivers/i2c/busses/i2c-tiny-usb.c .
+#make
+#make install
+#depmod -a
 
 # disable any text output on the LCD
 cat <<EOF > /boot/cmdline.txt
