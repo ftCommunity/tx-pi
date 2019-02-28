078c377fa8df56d39f19344216a15f3a5fc0a82d
Lars Heuer <heuer@semagia.com>
2019-02-28 23:35:58 +0100

    Added whitespaces

--- setup/tx-pi-setup.sh
+++ setup/tx-pi-setup.sh
@@ -153,7 +153,9 @@ de_DE.UTF-8 UTF-8
 nl_NL.UTF-8 UTF-8
 fr_FR.UTF-8 UTF-8
 EOF
+
 locale-gen
+
 #TODO: May fail if /etc/ssh/ssh_config contains "SendEnv LANG LC_*" (default) and the TX-PI setup is run headless via SSH
 update-locale --no-checks LANG="de_DE.UTF-8"
 
