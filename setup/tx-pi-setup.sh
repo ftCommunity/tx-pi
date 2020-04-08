abd7ded4c4291e2fa00238211d83d9f3dbfaaa8c
Lars <heuer@semagia.com>
2020-04-08 12:34:51 +0200

    Install Py3 wheel / setuptools

--- setup/tx-pi-setup.sh
+++ setup/tx-pi-setup.sh
@@ -120,6 +120,8 @@ if [ "$IS_STRETCH" = true ]; then
     pip3 install --upgrade setuptools
     pip3 install --upgrade wheel  # Needed for zbar
 else
+    apt-get -y install python3-setuptools
+    apt-get -y install python3-wheel
     apt-get -y install python3-semantic_version
     apt-get -y install python3-websockets
 fi
