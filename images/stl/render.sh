d8d1861b16166a4f37a784dec680e9051d528772
Lars <heuer@semagia.com>
2020-04-28 14:58:09 +0200

    Fixed path

--- images/stl/render.sh
+++ images/stl/render.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # for i in *.stl ; do ./render.sh $i ; done
-blender -b ~/bin/ft.blend -P ~/bin/viz.py -- $1 ${1%.stl}.png
+blender -b ./ft.blend -P ./viz.py -- $1 ${1%.stl}.png
 convert ${1%.stl}.png -fuzz 35% -trim +repage ${1%.stl}.png
