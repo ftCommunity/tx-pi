1807790d93ca51aa793d5d8cdebe5eb020b54005
Lars <heuer@semagia.com>
2020-05-18 12:27:16 +0200

    Don't downgrade libusb, won't fix issue #45

--- setup/tx-pi-setup.sh
+++ setup/tx-pi-setup.sh
@@ -116,18 +116,12 @@ if [ "$IS_BUSTER" = true ]; then
     cat <<EOF > /etc/apt/sources.list.d/tx-pi.list
 # TX-Pi sources used to solve issues with newer packages provided by Buster
 deb http://raspbian.raspberrypi.org/raspbian/ jessie main contrib non-free rpi
-deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi
 EOF
 
     cat <<EOF > /etc/apt/preferences.d/tx-pi
 # Added due to issues with the X11 VNC server
 Package: x11vnc x11vnc-data
 Pin: release n=jessie
-Pin-Priority: 1000
-
-# Added due to issues with libroboint and Raspberry Pi 4
-Package: libusb-0.1-4 libusb-dev
-Pin: release n=stretch
 Pin-Priority: 1000
 EOF
 fi
