86147137d857a0892a0944213809dae0495023d9
Lars <heuer@semagia.com>
2020-06-02 00:37:29 +0200

    Added missing Koma-Script styles

--- manual/setup.sh
+++ manual/setup.sh
@@ -2,4 +2,4 @@
 #
 # Script to create an initial setup for the manual
 #
-apt-get -y install texlive-luatex texlive-extra-utils texlive-fonts-extra lmodern texlive-lang-german
+apt-get -y install texlive-luatex texlive-extra-utils texlive-fonts-extra lmodern texlive-lang-german texlive-latex-extra
