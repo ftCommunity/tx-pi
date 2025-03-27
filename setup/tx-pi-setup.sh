ccc75841e96de8952919caf9d6639525496947cc
Lars <heuer@semagia.com>
2025-03-27 01:16:17 +0100

    Fixed another /boot/cmdline vs /boot/firmare issue

--- setup/tx-pi-setup.sh
+++ setup/tx-pi-setup.sh
@@ -179,7 +179,7 @@ fi
 # Driver installation changes "console=serial0,115200" to "console=ttyAMA0,115200"
 # Revert it here since /dev/ttyAMA0 is Bluetooth (Pi3, Pi3B+ ...)
 sed -i "s/=ttyAMA0,/=serial0,/g" /boot/firmware/cmdline.txt
-cmd_line=$( cat /boot/cmdline.txt )
+cmd_line=$( cat /boot/firmware/cmdline.txt )
 # Driver installation removes "fsck.repair=yes"; revert it
 if [[ $cmd_line != *"fsck.repair=yes"* ]]; then
     cmd_line="$cmd_line fsck.repair=yes"
@@ -190,7 +190,6 @@ EOF
 
 
 #-- Support for the TX-Pi HAT
-# Enable I2c
 header "Enable I2C"
 raspi-config nonint do_i2c 0 dtparam=i2c_arm=on
 sed -i "s/dtparam=i2c_arm=on/dtparam=i2c_arm=on\ndtparam=i2c_vc=on/g" /boot/firmware/config.txt
@@ -201,7 +200,7 @@ sed -i "s/dtparam=i2c_arm=on/dtparam=i2c_arm=on\ndtparam=i2c_vc=on/g" /boot/firm
 echo "dtoverlay=gpio-poweroff,gpiopin=4,active_low=1" >> /boot/firmware/config.txt
 
 
-#-- Enable WLAN
+header "Enable WLAN"
 nmcli radio wifi on
 
 
